7040eed02e350e3733cb753f36fb06d7756c8ec7
Ra-tnt <66058612+Ra-tnt@users.noreply.github.com>
2020-10-27 09:56:26 +0300

    Update login_test.js

--- e2e-js-test/login_test.js
+++ e2e-js-test/login_test.js
@@ -11,8 +11,8 @@
       casper.waitUntilVisible("#login-modal", function() {
         test.assertVisible("#login-modal", "user is presented with the login dialogue");
         this.fill("#login-modal form", {
-          "username": "Rghd",
-          "password": "1234"
+          "username": "Eve_Berger",
+          "password": "eve"
         }, false);
       }, function() {
         test.fail("login dialogue never showed up");
