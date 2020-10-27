758f2872ecddd59a05b6a196e5150c9d0149888c
Ra-tnt <66058612+Ra-tnt@users.noreply.github.com>
2020-10-27 09:17:12 +0300

    Update homepage_test.js

--- e2e-js-test/homepage_test.js
+++ e2e-js-test/homepage_test.js
@@ -8,7 +8,7 @@
       test.assertTitle("WeaveSocks", "homepage title is the one expected");
       test.assertTextExists("Login", "login link is present");
       test.assertNotVisible("ul.menu li.howdy", "user is not logged in");
-      test.assertTextExists("get a pet hamster for free!", "user is presented with the offer of the day");
+      test.assertTextExists("get a shoe for free!", "user is presented with the offer of the day");
       test.assertTextExists("0 items in cart", "cart is empty");
     });
 
