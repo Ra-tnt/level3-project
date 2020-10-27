c244c509177c67625cabe9d14d03cb94f22968a8
Ra-tnt <roody.tnt@gmail.com>
2020-10-27 08:55:48 +0300

    hi

--- e2e-js-test/homepage_test.js
+++ e2e-js-test/homepage_test.js
@@ -8,8 +8,8 @@
       test.assertTitle("WeaveSocks", "homepage title is the one expected");
       test.assertTextExists("Login", "login link is present");
       test.assertNotVisible("ul.menu li.howdy", "user is not logged in");
+      test.assertTextExists("Buy 1000 socks, get a shoe for free!", "user is presented with the offer of the day");
       test.assertTextExists("0 items in cart", "cart is empty");
-      test.assertTextExists("get a pet hamster for free!", "user is presented with the offer of the day");
     });
 
     casper.run(function() {
